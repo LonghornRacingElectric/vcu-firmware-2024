14c043fe1821f0057c55cfdb071f207c996fc83a
yashkmittal <yash198km@gmail.com>
2023-09-24 17:56:14 -0500

    addition: Added scripts beforemx and aftermx
    * Run beforeMX before using CUBEMX so that it does not
    reconfigure a new main.c file
    * Run afterMX after using CubeMX so that it returns to
    base state, removing all linking to now removed main.c

---
+++ aftermx.py
@@ -0,0 +1,6 @@
+import os
+import subprocess
+
+subprocess.run(["git", "restore", ".mxproject"])
+subprocess.run(["git", "restore", "CMakeLists.txt"])
+os.rename("Core/Src/main.c", "Core/Src/main.cpp")
