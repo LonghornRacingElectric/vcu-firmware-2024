3f4afed51b7582eaf054deb87a24fdc77d9b296f
matt-mekha <matthew.mekha@gmail.com>
2023-11-30 15:58:17 -0600

    led

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -52,8 +52,7 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, OUT_LED_R_Pin|OUT_LED_G_Pin|OUT_LED_B_Pin|OUT_ReadyToDrive_Pin
-                          |SPI_CS_EEPROM_Pin|OUT_AMS_LED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, OUT_ReadyToDrive_Pin|SPI_CS_EEPROM_Pin|OUT_AMS_LED_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(SPI_CS_IMU_GPIO_Port, SPI_CS_IMU_Pin, GPIO_PIN_RESET);
@@ -69,10 +68,8 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PAPin PAPin PAPin PAPin
-                           PAPin PAPin */
-  GPIO_InitStruct.Pin = OUT_LED_R_Pin|OUT_LED_G_Pin|OUT_LED_B_Pin|OUT_ReadyToDrive_Pin
-                          |SPI_CS_EEPROM_Pin|OUT_AMS_LED_Pin;
+  /*Configure GPIO pins : PAPin PAPin PAPin */
+  GPIO_InitStruct.Pin = OUT_ReadyToDrive_Pin|SPI_CS_EEPROM_Pin|OUT_AMS_LED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
