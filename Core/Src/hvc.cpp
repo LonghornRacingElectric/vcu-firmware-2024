c67eb0aff7c5ad414dcfc54e7145f00c6b0a8f55
CooperNelson16 <cjnelson016@gmail.com>
2023-12-03 18:55:31 -0600

    in progress for adding CAN mailbox stuff

--- Core/Src/hvc.cpp
+++ Core/Src/hvc.cpp
@@ -1,8 +1,16 @@
 #include "hvc.h"
+#include "angel_can.h"
+
+static CanRx Hvc_mailbox_voltage;
+static CanRx Hvc_mailbox_current;
+static CanRx Hvc_mailbox_batteryTemp;
+static CanRx Hvc_mailbox_imd;
+static CanRx Hvc_mailbox_ams;
 
 
 void hvc_init() {
-    // TODO implement
+    can_addMailbox(HVC_VCU_INFO_VOLTAGE,&Hvc_mailbox_voltage);
+
 }
 
 void hvc_getStatus(HvcStatus* status) {
--- Core/Src/wheelspeeds.cpp
+++ Core/Src/wheelspeeds.cpp
@@ -1,10 +1,27 @@
 #include "wheelspeeds.h"
+#include "angel_can.h"
+
+static CanRx WheelSpeed_mailbox_FL;
+static CanRx WheelSpeed_mailbox_FR;
+static CanRx WheelSpeed_mailbox_BL;
+static CanRx WheelSpeed_mailbox_BR;
 
 
 void wheelspeeds_init() {
+    can_addMailbox(WHS_VCU_INFO_FL, &WheelSpeed_mailbox_FL);
+    can_addMailbox(WHS_VCU_INFO_FR, &WheelSpeed_mailbox_FR);
+    can_addMailbox(WHS_VCU_INFO_BL, &WheelSpeed_mailbox_BL);
+    can_addMailbox(WHS_VCU_INFO_BR, &WheelSpeed_mailbox_BR);
 
 }
 
 void wheelspeeds_get(WheelDisplacements* wheelDisplacements) {
+    //CAN data is 4 bytes, first two are the wheel speed, the 3rd 4th are currently undetermined.
+
+    //TODO determine if this will return fixed point or smth (if i have to convert the raw data)
+    wheelDisplacements->fl = (float) can_readBytes(WheelSpeed_mailbox_FL.data, 0, 1); //
+    wheelDisplacements->fr = (float) can_readBytes(WheelSpeed_mailbox_FR.data, 0, 1); //
+    wheelDisplacements->bl = (float) can_readBytes(WheelSpeed_mailbox_BL.data, 0, 1); //
+    wheelDisplacements->br = (float) can_readBytes(WheelSpeed_mailbox_BR.data, 0, 1); //
 
 }
