b7fa655ba1b3a7feb70c59d64534354a0b0a54c8
matt-mekha <matthew.mekha@gmail.com>
2023-12-13 14:31:50 -0600

    hvc fix

--- Core/Src/hvc.cpp
+++ Core/Src/hvc.cpp
@@ -19,34 +19,41 @@ void hvc_init() {
     can_addMailbox(HVC_VCU_CONTACTOR_STATUS, &hvc_contactor_status_mailbox);
 }
 
-uint32_t hvc_coolingRequest_send(bool battfan_turn_on, uint16_t battfan_rpm_req, uint16_t battfan_unique_rpm_req) {
-    uint8_t data[5];
-    data[0] = (uint8_t) battfan_turn_on;
-    can_writeBytes(data, 1, 2, battfan_rpm_req );
-    can_writeBytes(data, 3, 4, battfan_unique_rpm_req );
+uint32_t hvc_setCoolingOutput(uint16_t battFanRpm, uint16_t battUniqueSegRpm) {
+    uint8_t data[4];
+    can_writeBytes(data, 0, 1, battFanRpm);
+    can_writeBytes(data, 2, 3, battUniqueSegRpm);
 
-    return can_send(VCU_HVC_COOLING, 5, data);
+    return can_send(VCU_HVC_COOLING, 4, data);
 }
 
-void hvc_getStatus(HvcStatus* status) {
-    if(hvc_ams_imd_mailbox.isRecent){ // Demand packet receive (when amd / imd changes)
+void hvc_periodic(HvcStatus* status, VcuOutput* vcuOutput, float deltaTime) {
+    static float t = 0;
+    t += deltaTime;
+    if(t > 0.25f) {
+        t = 0;
+        // TODO vcuOutput->battFanRpm (need to update VCU core)
+        hvc_setCoolingOutput(1000, 1000);
+    }
+
+    if(hvc_ams_imd_mailbox.isRecent){
         status->ok = true;
         status->imd = (bool) can_readBytes(hvc_ams_imd_mailbox.data, 0, 0);
         status->ams = (bool) can_readBytes(hvc_ams_imd_mailbox.data, 1, 1);
         hvc_ams_imd_mailbox.isRecent = false;
         status->isRecent = true;
     }
-    if(hvc_pack_status_mailbox.isRecent) { // Continuous packet receive (every 3 ms)
+    if(hvc_pack_status_mailbox.isRecent) {
         status->ok = true;
-        status->pack_voltage = (float) can_readBytes(hvc_pack_status_mailbox.data, 0, 1) / 10.0f;
-        status->pack_current = (float) can_readBytes(hvc_pack_status_mailbox.data, 2, 3) / 10.0f;
+        status->packVoltage = (float) can_readBytes(hvc_pack_status_mailbox.data, 0, 1) / 10.0f;
+        status->packCurrent = (float) can_readBytes(hvc_pack_status_mailbox.data, 2, 3) / 10.0f;
         status->stateOfCharge = (float) can_readBytes(hvc_pack_status_mailbox.data, 4, 5) / 10.0f;
         hvc_pack_status_mailbox.isRecent = false;
         status->isRecent = true;
     }
-    if(hvc_contactor_status_mailbox.isRecent) { // Demand packet receive (when Contactor status changes)
+    if(hvc_contactor_status_mailbox.isRecent) {
         status->ok = true;
-        status->contactor_status = (HvcStatus::ContactorStatus) hvc_contactor_status_mailbox.data[0];
+        status->contactorStatus = (HvcStatus::ContactorStatus) hvc_contactor_status_mailbox.data[0];
         hvc_contactor_status_mailbox.isRecent = false;
         status->isRecent = true;
     }
