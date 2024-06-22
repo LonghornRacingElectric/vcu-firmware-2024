2da256c16d14346480eb0ed76958c0a2e17b34d8
matt-mekha <matthew.mekha@gmail.com>
2024-06-21 21:20:05 -0700

    no more inverter tuning

--- Core/Src/inverter.cpp
+++ Core/Src/inverter.cpp
@@ -18,35 +18,24 @@ static CanInbox inverterStateInbox;
 static CanInbox inverterFaultInbox;
 static CanInbox torqueInfoInbox;
 static CanInbox highSpeedInbox;
-static CanInbox paramsResponseInbox;
-static CanInbox extraInbox;
 
 static CanOutbox torqueCommandOutbox;
-static CanOutbox paramsRequestOutbox;
-
-typedef struct ParameterUpdate {
-  uint16_t address;
-  uint16_t value;
-  float timeRemaining;
-} ParameterUpdate;
-
-static queue<ParameterUpdate> parameterUpdateQueue;
 
 void inverter_init() {
   can_addInbox(INV_VOLTAGE, &voltageInbox, INV_TIMEOUT_FAST);
   can_addInbox(INV_CURRENT, &currentInbox, INV_TIMEOUT_FAST);
   can_addInbox(INV_TEMP3_DATA, &motorTempInbox, INV_TIMEOUT_SLOW);
-  can_addInbox(0x0A1, &extraInbox, INV_TIMEOUT_SLOW);
   can_addInbox(INV_TEMP1_DATA, &inverterTempInbox, INV_TIMEOUT_SLOW);
   can_addInbox(INV_MOTOR_POSITIONS, &motorPosInbox, INV_TIMEOUT_FAST);
   can_addInbox(INV_STATE_CODES, &inverterStateInbox, INV_TIMEOUT_FAST);
   can_addInbox(INV_FAULT_CODES, &inverterFaultInbox, INV_TIMEOUT_FAST);
   can_addInbox(INV_TORQUE_TIMER, &torqueInfoInbox, INV_TIMEOUT_FAST);
   //can_addInbox(INV_HIGH_SPEED_MSG, &highSpeedInbox, INV_TIMEOUT_VERYFAST);
-  can_addInbox(INV_VCU_PARAMS_RESPONSE, &paramsResponseInbox, 1.0f);
 
   can_addOutbox(VCU_INV_COMMAND, 0.003f, &torqueCommandOutbox);
 
+
+//  inverter_writeParameter(147, 1000); // CAN bit rate
 
 //  inverter_writeParameter(148, 0x1CE5); // message selection
 //  inverter_writeParameter(168, 10); // torque ramp, use 100 loaded
@@ -62,18 +51,11 @@ void inverter_init() {
 //  inverter_writeParameter(166, 0); // D gain
 //  inverter_writeParameter(167, 0); // low-pass filter gain
 //  inverter_writeParameter(187, 0); // shudder compensation
-//  inverter_writeParameter(241, 10); // default PWM
-//  inverter_writeParameter(246, 10); // min PWM
-//  inverter_writeParameter(247, 10); // max PWM
-//  inverter_writeParameter(245, 10); // stall PWM
+//  inverter_writeParameter(241, 12); // default PWM
+//  inverter_writeParameter(246, 12); // min PWM
+//  inverter_writeParameter(247, 12); // max PWM
+//  inverter_writeParameter(245, 12); // stall PWM
 //  inverter_writeParameter(250, 0); // nominal PWM mode with stall region
-  can_writeInt(uint16_t, &paramsRequestOutbox, 0, 20); // param addr
-  can_writeInt(uint8_t, &paramsRequestOutbox, 2, 1); // write
-  can_writeInt(uint16_t, &paramsRequestOutbox, 4, 0); // param value
-  can_addOutbox(0x0C1, 0.1f, &paramsRequestOutbox);
-
-//  inverter_writeParameter(147, 1000); // CAN bit rate
-
 
   // 246 = 6 = 6 kHz min PWM
   // 247 = 17 = 17 kHz max PWM
@@ -82,11 +64,6 @@ void inverter_init() {
 }
 
 static void inverter_getStatus(InverterStatus *status) {
-
-  if(extraInbox.isRecent) {
-    volatile float x = can_readFloat(int16_t, &extraInbox, 0, 0.1f);
-    x++;
-  }
 
   if (inverterTempInbox.isRecent) {
     auto temp = can_readFloat(int16_t, &inverterTempInbox, 0, 0.1f) +
@@ -163,11 +140,6 @@ static void inverter_getStatus(InverterStatus *status) {
   else {
     FAULT_CLEAR(&faultVector, FAULT_VCU_INV);
   }
-
-  if (paramsResponseInbox.isRecent) {
-    status->newData = can_readInt(uint16_t, &paramsResponseInbox, 4);
-    paramsResponseInbox.isRecent = false;
-  }
 }
 
 
@@ -182,56 +154,7 @@ static void inverter_updateTorqueCommand(float torque, float rpm, bool enable_in
   torqueCommandOutbox.isRecent = true;
 }
 
-void inverter_resetFaults() {
-  // send a can message telling the inverter to reset faults by setting addr 20 to 0
-  return inverter_writeParameter(20, 0);
-}
-
-void inverter_writeParameter(uint16_t address, uint16_t value) {
-  ParameterUpdate parameterUpdate = {
-    address, value, 1.0f
-  };
-  parameterUpdateQueue.push(parameterUpdate);
-}
-
-void inverter_readParameter(uint16_t address) {
-  can_writeInt(uint16_t, &paramsRequestOutbox, 0, address); // param addr
-  can_writeInt(uint8_t, &paramsRequestOutbox, 2, 0); // read
-  paramsRequestOutbox.dlc = 8;
-}
-
-void inverter_updateParameterCommand(float deltaTime) {
-  if (parameterUpdateQueue.empty()) {
-    return;
-  }
-
-  ParameterUpdate parameterUpdate = parameterUpdateQueue.front();
-
-  // send a can message telling the inverter to set params
-  can_writeInt(uint16_t, &paramsRequestOutbox, 0, parameterUpdate.address); // param addr
-  can_writeInt(uint8_t, &paramsRequestOutbox, 2, 1); // write
-  can_writeInt(uint16_t, &paramsRequestOutbox, 4, parameterUpdate.value); // param value
-  paramsRequestOutbox.dlc = 8;
-
-  // weird inconsistency in cascadia documentation
-  if(parameterUpdate.address == 148) {
-    paramsRequestOutbox.data[6] = 0xFE;
-    paramsRequestOutbox.data[7] = 0xFF;
-  } else {
-    paramsRequestOutbox.data[6] = 0;
-    paramsRequestOutbox.data[7] = 0;
-  }
-
-  parameterUpdate.timeRemaining -= deltaTime;
-  if(parameterUpdate.timeRemaining <= 0.0f && clock_getTime() > 3.0f) {
-    parameterUpdateQueue.pop();
-  }
-}
-
 void inverter_periodic(InverterStatus *status, VcuOutput* vcuCoreOutput, float deltaTime) {
   inverter_getStatus(status);
   inverter_updateTorqueCommand(vcuCoreOutput->inverterTorqueRequest, 0, vcuCoreOutput->enableInverter);
-//  inverter_updateParameterCommand(deltaTime);
-//  inverter_readParameter(250);
-//  println(status->newData);
 }
