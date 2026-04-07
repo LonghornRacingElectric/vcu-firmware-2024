809eba04c408e15cbb482c506c23be50e64b0832
[Andrew Cloran] <[andrew@cloran.com]>
2026-04-07 04:35:39 -0500

    Trying more to fix wheel speeds

--- Core/Inc/inverter.h
+++ Core/Inc/inverter.h
@@ -14,11 +14,15 @@ typedef struct InverterStatus {
   float motorTemp;
 
   float motorAngle;
-  float resolverAngle;
+  float deltaResolverFiltered;
 
   float phaseACurrent;
   float phaseBCurrent;
   float phaseCCurrent;
+  float idFeedback;
+  float iqFeedback;
+  float idCommand;
+  float iqCommand;
 
   float BCVoltage;
   float ABVoltage;
@@ -31,6 +35,16 @@ typedef struct InverterStatus {
 
   uint64_t faultVector;
   uint64_t stateVector;
+  bool bmsLimitingRegenTorque;
+  bool limitMotorTempDerate;
+  bool limitHotSpotMotor;
+  bool bmsActive;
+  bool bmsLimitingMotorTorque;
+  bool limitMaxSpeed;
+  bool limitHotSpotInverter;
+  bool lowSpeedLimiting;
+  bool limitCoolantDerating;
+  bool limitStallBurstModel;
 
   uint32_t newData; // Comes from params in inverter.
 } InverterStatus;
